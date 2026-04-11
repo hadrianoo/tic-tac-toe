1b7b63f4fe07693c1d963e7e8fbcd7eca340cb6a
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-04-11 19:29:30 +0200

    In progess: work on game logic

--- scripts.js
+++ scripts.js
@@ -9,19 +9,68 @@ const gameBoard = (() => {
         return board;
     };
 
-    const setBoard = (arr, item, playerSymbol) => {
+    const getBoardPosition = (arr, item) => board[arr][item];
+
+    const updateBoard = (arr, item, playerSymbol) => {
         board[arr][item] = playerSymbol;
+
+        checkBoardStatus(playerSymbol);
+
     }
+
+    const checkBoardStatus = (playerSymbol) => {
+        if (checkRowStatus(playerSymbol) || checkColStatus(playerSymbol) || checkDiagonalStatus(playerSymbol)) {
+            console.log(`player ${playerSymbol} won`)
+        }
+
+    }
+
+    const checkRowStatus = (symbol) => {
+        getBoard().forEach(row => {
+            let symbolCount = 0;
+            row.forEach(item => {
+                if (item === symbol) {
+                    symbolCount++;
+                }
+            })
+            if (symbolCount === 3) {
+                return true;
+            }
+        })
+    }
+
+    const checkColStatus = (symbol) => {
+        for (let i = 0; i <= 3; i++) {
+            if (getBoard()[0][i] === symbol &&
+                getBoard()[1][i] === symbol &&
+                getBoard()[2][i] === symbol) {
+                return true;
+            }
+        }
+    }
+
+    const checkDiagonalStatus = (symbol) => {
+
+        if (getBoard()[0][0] === symbol &&
+            getBoard()[1][1] === symbol &&
+            getBoard()[2][2] === symbol) {
+            return true;
+        }
+        if (getBoard()[0][2] === symbol &&
+            getBoard()[1][1] === symbol &&
+            getBoard()[2][0] === symbol) {
+            return true;
+        }
+
+    }
+
 
     return {
         getBoard,
-        setBoard,
+        getBoardPosition,
+        updateBoard,
     }
 })();
-
-
-console.log(gameBoard.setBoard(0, 1, "X"))
-console.table(gameBoard.getBoard());
 
 
 function createPlayer(name) {
@@ -41,5 +90,38 @@ function createPlayer(name) {
 
 const game = (() => {
 
+    let playerChoiceRow = 0;
+    let playerChoiceCol = 0;
+    let player = "";
+
+
+    const checkBoardPosition = () => {
+        if (gameBoard.getBoardPosition(playerChoiceRow, playerChoiceCol) === 0) {
+            gameBoard.updateBoard(playerChoiceRow, playerChoiceCol, player);
+        } else {
+            console.log("This position it taken, try again");
+        }
+    }
+
+
+    const setPlayerChoice = (arr, item, playerSymbol) => {
+        playerChoiceRow = arr;
+        playerChoiceCol = item;
+        player = playerSymbol;
+        checkBoardPosition();
+        console.table(gameBoard.getBoard())
+
+    }
+
+    return {
+        setPlayerChoice,
+
+    }
+
 })();
 
+
+
+game.setPlayerChoice(0, 1, "X");
+game.setPlayerChoice(1, 1, "X");
+game.setPlayerChoice(2, 1, "X");
